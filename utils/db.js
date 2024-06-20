565510f0342c00e7eb74378cc1c35738d6630302
VickArmand <victormaina1962@gmail.com>
2024-06-20 03:59:47 +0300

    1. MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -1,23 +1,29 @@
 import MongoClient from "mongodb/lib/mongo_client";
 class DBClient {
     constructor() {
-        this.host = process.env.DB_HOST  | 'localhost';
-        this.port = process.env.DB_PORT | 27017;
-        this.dbName = process.env.DB_DATABASE | 'files_manager';
+        this.host = process.env.DB_HOST  || 'localhost';
+        this.port = process.env.DB_PORT || 27017;
+        this.dbName = process.env.DB_DATABASE || 'files_manager';
         this.url = `mongodb://${this.host}:${this.port}`;
-        this.isConnected = true;
+        this.isConnected = false;
+        this.db = null;
         this.client = new MongoClient(this.url);
+        this.client.connect((err) => {
+            if (!err) {
+                this.isConnected = true;
+                this.db = this.client.db(this.dbName);
+            }
+        });
     }
     isAlive() {
-        this.client.connect().catch((err) => {
-            this.isConnected = false;
-        })
         return this.isConnected;
     }
     async nbUsers() {
-
+        return this.db.collection('users').countDocuments();
     }
     async nbFiles() {
-
+        return this.db.collection('files').countDocuments();
     }
 }
+const dbclient = new DBClient();
+export default dbclient;
