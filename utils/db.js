99607989212a9dc039ae8c2980202e9871efb67f
VickArmand <victormaina1962@gmail.com>
2024-06-21 14:06:36 +0300

    eslint corrections

--- utils/db.js
+++ utils/db.js
@@ -1,29 +1,33 @@
-import MongoClient from "mongodb/lib/mongo_client";
+import MongoClient from 'mongodb/lib/mongo_client';
+
 class DBClient {
-    constructor() {
-        this.host = process.env.DB_HOST  || 'localhost';
-        this.port = process.env.DB_PORT || 27017;
-        this.dbName = process.env.DB_DATABASE || 'files_manager';
-        this.url = `mongodb://${this.host}:${this.port}`;
-        this.isConnected = false;
-        this.db = null;
-        this.client = new MongoClient(this.url);
-        this.client.connect((err) => {
-            if (!err) {
-                this.isConnected = true;
-                this.db = this.client.db(this.dbName);
-            }
-        });
-    }
-    isAlive() {
-        return this.isConnected;
-    }
-    async nbUsers() {
-        return this.db.collection('users').countDocuments();
-    }
-    async nbFiles() {
-        return this.db.collection('files').countDocuments();
-    }
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.dbName = process.env.DB_DATABASE || 'files_manager';
+    this.url = `mongodb://${this.host}:${this.port}`;
+    this.isConnected = false;
+    this.db = null;
+    this.client = new MongoClient(this.url);
+    this.client.connect((err) => {
+      if (!err) {
+        this.isConnected = true;
+        this.db = this.client.db(this.dbName);
+      }
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
 }
 const dbclient = new DBClient();
 export default dbclient;
--- utils/redis.js
+++ utils/redis.js
@@ -1,29 +1,33 @@
 /** */
 import redis from 'redis';
+
 class RedisClient {
-    constructor() {
-       this.client = redis.createClient();
-       this.isConnected = false;
-       this.client.on('error', (err) => {
-        console.log(err);
-       });
-       this.client.on('connect', () => {
-        this.isConnected = true;
-       });
-    }
-    isAlive() {
-        return this.isConnected;
-    }
-    async get(key) {
-        return this.client.get(key);
-    }
-    async set(key, value, duration) {
-        return this.client.set(key, value, {EX: duration})
-    }
-    async del(key) {
-        return this.client.del(key)
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.isConnected = false;
+    this.client.on('error', (err) => {
+      console.log(err);
+    });
+    this.client.on('connect', () => {
+      this.isConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(key) {
+    return this.client.get(key);
+  }
+
+  async set(key, value, duration) {
+    return this.client.set(key, value, { EX: duration });
+  }
+
+  async del(key) {
+    return this.client.del(key);
+  }
 }
 const redisClient = new RedisClient();
 export default redisClient;
-
